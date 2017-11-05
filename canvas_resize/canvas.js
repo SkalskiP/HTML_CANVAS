8b7489025a4c7f4e22e5a7c877430f5209ced722
SkalskiP <pskalski@student.agh.edu.pl>
2017-11-05 14:46:28 +0100

    lines, circles, rect

--- canvas_resize/canvas.js
+++ canvas_resize/canvas.js
@@ -7,8 +7,34 @@ canvas.height = window.innerHeight;
 // allows to create geometric objects on 2d canvas
 var context = canvas.getContext('2d');
 
+// Rectangle drawing
+context.fillStyle = "rgba(255, 0, 0, 0.5)";
 context.fillRect(100, 100, 100, 100);
+context.fillStyle = "rgba(0, 0, 255, 0.5)";
 context.fillRect(200, 200, 100, 100);
+context.fillStyle = "rgba(0, 255, 0, 0.5)";
 context.fillRect(300, 300, 100, 100);
 
-console.log(canvas);
+// Line drawing
+context.beginPath();
+context.moveTo(50, 300);
+context.lineTo(300, 100);
+context.lineTo(400, 300);
+context.strokeStyle = "#fa34a3";
+context.stroke();
+
+// Arc / Circle drawing
+// context.beginPath();
+// context.arc(300, 300, 30, 0, Math.PI * 2, false);
+// context.strokeStyle = 'blue';
+// context.stroke();
+
+for (var i = 0; i < 10; i++) {
+    var x = Math.random() * window.innerWidth;
+    var y = Math.random() * window.innerHeight;
+
+    context.beginPath();
+    context.arc(x, y, 30, 0, Math.PI * 2, false);
+    context.strokeStyle = 'blue';
+    context.stroke();
+}
